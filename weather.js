f23d087390e3fed65558a16bee4fe2e7ea4c6cf7
Trey Ritter <trey353@gmail.com>
2018-04-23 16:51:30 -0400

    Converted functions to es6 format

--- weather.js
+++ weather.js
@@ -2,13 +2,13 @@ const https = require('https');
 const api = require('./api.json');
 
 // Print out temp details
-function printWeather(weather) {
+const printWeather = (weather) => {
     const message = `Current temperature in ${weather.location.city} is ${weather.current_observation.temp_f}F`;
     console.log(message);
 }
 // Print out error message
 
-function get(query) {
+const get = (query) => {
     const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`, response => {
         let body = "";
         // Read the data
